4657b88cd9fe440c9fca1fd4207e0a6861206094
sudaisansari <ansarisudais333@gmail.com>
2023-08-31 23:13:29 +0500

    final changes

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,45 +2,31 @@ import Logo from "/public/logo.png"
 import Image from "next/image"
 import Link from "next/link"
 import Wrapper from "@/components/shared/Wrapper"
-import add from "@/app/page"
-
-interface NavItem {
-    label: string
-    page: string
-  }
-  
-  const NAV_ITEMS: Array<NavItem> = [
-    {
-      label: "hero",
-      page: "hero",
-    },
-    {
-      label: "specializedTracks",
-      page: "specializedTracks",
-    }
-  ]
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-10 bg-white">
-        <Wrapper>
-            <div className="flex justify-between py-4 items-center">
-                {/* Logo */}
-                <div className="object-scale-down h-12 w-24">
-                    {/* <h2 className="text-xl font-bold">Panaverse dao</h2> */}
-                    <Image src={Logo} alt="Panaverse dao logo" />
+        <header className="sticky top-0 z-10 bg-white" id="navbar">
+            <Wrapper>
+                <div className="flex justify-between py-4 items-center">
+                    {/* Logo */}
+                    <div className="object-scale-down h-12 w-24">
+                        <a href="#hero">
+                            {/* <h2 className="text-xl font-bold">Panaverse dao</h2> */}
+                            <Image src={Logo} alt="Panaverse dao logo" />
+                        </a>
+                    </div>
+
+                    {/* Navigation bar */}
+                    <ul className="flex space-x-8 font-medium">
+                        <li>
+                            <a href={"#hero"}>Home</a>
+                        </li>
+                        <li>
+                            <a href={"#specializedTracks"}>Courses</a>
+                        </li>
+                    </ul>
                 </div>
-                {/* Navigation bar */}
-                <ul className="flex space-x-8 font-medium">
-                    <li>
-                        <Link href={"/"}>Home</Link>
-                    </li>
-                    <li>
-                        <Link href={"cours/components/widgetsORsections/SpecializedTrackes"}>Courses</Link>
-                    </li>
-                </ul>
-            </div>
-        </Wrapper>
+            </Wrapper>
         </header>
     )
 }
--- src/components/widgetsORsections/ProgramOutcome.tsx
+++ src/components/widgetsORsections/ProgramOutcome.tsx
@@ -1,6 +1,7 @@
 import Wrapper from "../shared/Wrapper"
 import Image from "next/image"
 import ProgramImage from "@/assets/images/Program Outcome.png"
+import { GiCheckMark } from "react-icons/gi"
 
 const OutcomePoints = ["Product Ownership", "Freelacing", "Global Marketing by  DAO", "Boosting Economy."];
 
@@ -22,20 +23,8 @@ const ProgramOutcome = () => (
                     </p>
                     <div className="mt-6 gap-x-2 gap-y-4 grid grid-cols-2">
                         {OutcomePoints.map((item, i) => (
-                            <div key={i} className="flex items-center gap-x-2">
-                                <svg width="46" className="flex-shrink-0" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.3431 2.65686C20.4673 -0.467337 25.5327 -0.46734 28.6569 2.65685L43.3431 17.3431C46.4673 20.4673 46.4673 25.5327 43.3431 28.6569L28.6569 43.3431C25.5327 46.4673 20.4673 46.4673 17.3431 43.3431L2.65686 28.6569C-0.467337 25.5327 -0.46734 20.4673 2.65685 17.3431L17.3431 2.65686Z" fill="url(#paint0_radial_0_1)" />
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M14 21.9206C16.0707 22.3461 18.734 23.1698 20.75 24.465C23.1178 21.3345 27.371 18.406 32 16C27.6042 20.2739 23.9668 25.1236 21.5 30C19.5118 27.2129 17.3398 24.6189 14 21.9206Z" fill="white" />
-                                    <defs>
-                                        <radialGradient id="paint0_radial_0_1" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(13.5455 -9.96429) rotate(59.1918) scale(77.3011 169.686)">
-                                            <stop offset="0.125" stopColor="#00E1F0" />
-                                            {/* Earlier when copied from figma stop-color change it to stopColor same with fillRule and clipRule*/}
-                                            <stop offset="1" stopColor="#00616C" />
-                                        </radialGradient>
-                                    </defs>
-                                </svg>
-
-
+                            <div key={i} className="flex items-center gap-x-3">
+                                <GiCheckMark className="text-[#06BF9C]" />
                                 <h3 className="font-medium text-lg">{item}</h3>
                             </div>
                         ))}
